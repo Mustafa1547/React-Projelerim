728511b80f1bdcc536319b514767708f48d329fa
Mustafa1547 <142215559+Mustafa1547@users.noreply.github.com>
2025-11-28 17:09:08 +0300

    Add files via upload

---
+++ course-app/src/Data.jsx
@@ -0,0 +1,41 @@
+import Csharp from './images/csharp.jpeg';
+import Java from './images/java.jpeg';
+import Sql from './images/sql.jpeg';
+import Web from './images/web.jpeg'
+
+
+export const courses = [
+    {
+        id: 1234,
+        title: "C# Proglamlama Seti",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores amet molestias nostrum? Tenetur nulla inventore repellat repellendus error dolore praesentium?",
+        price: 400,
+        link: "https://www.udemy.com/course/c-sharp-programlama-dili-her-seviyeye-hitap-eden-egitim-seti",
+        image: Csharp
+    },
+    {
+        id: 2345,
+        title: "Java Proglamlama Dili",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores amet molestias nostrum? Tenetur nulla inventore repellat repellendus error dolore praesentium?",
+        price: 450,
+        link: "https://www.udemy.com/course/java-programlama-dili-her-seviyeye-uygun-egitim-seti",
+        image: Java
+    },
+    {
+        id: 3456,
+        title: "Sql Öğreniyorum",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores amet molestias nostrum? Tenetur nulla inventore repellat repellendus error dolore praesentium?",
+        price: 300,
+        link: "https://www.udemy.com/course/uygulamarla-sql-server-ogreniyorum-adan-zye",
+        image: Sql
+    },
+        {
+        id: 4567,
+        title: "Modern Web Geliştirme Eğitim",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores amet molestias nostrum? Tenetur nulla inventore repellat repellendus error dolore praesentium?",
+        price: 350,
+        link: "https://www.udemy.com/course/modern-web-gelistirme-kursu-sifirdan-ileri-seviyeye",
+        image: Web
+    }
+
+]
